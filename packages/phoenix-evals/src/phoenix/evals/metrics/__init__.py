b5c788f71ea2f186c5fcc88e68636eaf003c4910
Siddharth Sahu <112792547+sahusiddharth@users.noreply.github.com>
2025-10-02 12:49:04 +0530

    Revert "feat(evals): add regex evaluator"

--- packages/phoenix-evals/src/phoenix/evals/metrics/__init__.py
+++ packages/phoenix-evals/src/phoenix/evals/metrics/__init__.py
@@ -1,13 +1,11 @@
 from .document_relevance import DocumentRelevanceEvaluator
 from .exact_match import exact_match
 from .hallucination import HallucinationEvaluator
-from .matches_regex import MatchesRegex
 from .precision_recall import PrecisionRecallFScore
 
 __all__ = [
     "exact_match",
-    "DocumentRelevanceEvaluator",
     "HallucinationEvaluator",
-    "MatchesRegex",
     "PrecisionRecallFScore",
+    "DocumentRelevanceEvaluator",
 ]
